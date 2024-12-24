e08262db80a7db9d9ee8cfe3136d1344c77610f3
AshkanFeyzollahi <ashkanfeyzollahi@gmail.com>
2024-12-24 22:40:24 +0330

    Rename build_vocabulary.py to build_dictionary.py

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -1,5 +1,5 @@
 """
-The script that used to build vocabularies with word frequency
+The script that used to build dictionaries with word frequency
 """
 
 import gzip
@@ -9,12 +9,12 @@ from pathlib import Path
 from faspellchecker.utils import is_persian_word
 
 # Find `fa-spellchecker/resources` folder
-vocabulary_resources = Path("resources/")
+dictionary_resources = Path("resources/")
 
 collected_contents = []
 
 # Get all resources from the `fa-spellchecker/resources` folder
-for resource in vocabulary_resources.glob("./*"):
+for resource in dictionary_resources.glob("./*"):
     # Open and read resource file
     with open(resource) as f:
         # And add the content of resource file to `collected_contents`
@@ -23,23 +23,23 @@ for resource in vocabulary_resources.glob("./*"):
 # Convert `collected_contents` into a string of words those were collected from resources
 collected_contents = "\n".join(collected_contents)
 
-vocabulary_json = {}
+dictionary_json = {}
 
 # Split collected contents into a list of words
 for word in collected_contents.split():
-    # If the word is already in `vocabulary_json`, then just increase its frequency
-    if word in vocabulary_json:
-        vocabulary_json[word] = vocabulary_json[word] + 1
+    # If the word is already in `dictionary_json`, then just increase its frequency
+    if word in dictionary_json:
+        dictionary_json[word] = dictionary_json[word] + 1
 
         continue
 
     # If not, then check if the word is a persian word
     if is_persian_word(word):
-        # If it's, then add it to `vocabulary_json`
-        vocabulary_json[word] = 1
+        # If it's, then add it to `dictionary_json`
+        dictionary_json[word] = 1
 
 # Then open a gzip-compressed file
-vocabulary_gzip_file = gzip.open("faspellchecker/fa-vocabulary.json.gz", "wt")
+dictionary_gzip_file = gzip.open("faspellchecker/fa-dictionary.json.gz", "wt")
 
 # Then save collected words into it!
-json.dump(vocabulary_json, vocabulary_gzip_file)
+json.dump(dictionary_json, dictionary_gzip_file)
